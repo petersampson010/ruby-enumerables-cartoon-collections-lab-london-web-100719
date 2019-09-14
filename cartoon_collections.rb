ecdc9eac69b6597c84c596be1275943eaacd36b2
Peter Sampson <petersampson010@gmail.com>
2019-09-14 13:37:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
   array.each_with_index { |i, index| p"#{index+1} #{i}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(plant)
+  plant.map
 end
 
 def long_planeteer_calls# code an argument here
